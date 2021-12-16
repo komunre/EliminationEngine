f6367fdb133f299f6bcc9e42e465f96194d8e8b3
komunre <koliziy5@gmail.com>
2021-12-16 14:45:11 +0700

    Start

---
+++ EliminationEngine/Elimination.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EliminationEngine
+{
+    public class Elimination
+    {
+        public string Title { get; set; } = "Elimination";
+        public void Run()
+        {
+
+        }
+    }
+}
