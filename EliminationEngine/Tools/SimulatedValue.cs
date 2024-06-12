5747536f997a1b3e88fdd0c423176bebd64cad87
komunre <koliziy5@gmail.com>
2024-06-12 21:31:05 +0700

    SimulatedValue updated

--- EliminationEngine/Tools/SimulatedValue.cs
+++ EliminationEngine/Tools/SimulatedValue.cs
@@ -1,16 +1,18 @@
-﻿using System;
+﻿using OpenTK.Mathematics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
+using static EliminationEngine.Tools.Interpolator;
 
 namespace EliminationEngine.Tools
 {
     public class SimulatedValue
     {
-        private float _currentValue;
-        private float _desiredValue;
+        protected float _currentValue;
+        protected float _desiredValue;
 
         public float CurrentValue { get; set; }
         public float DesiredValue
@@ -75,6 +77,60 @@ namespace EliminationEngine.Tools
         public Interpolator.InterpolationProcedure GetInterpolationProcedure()
         {
             return _interpolationProcedure;
+        }
+    }
+
+    public class SimulatedVectorValue
+    {
+        protected SimulatedValue X;
+        protected SimulatedValue Y;
+        protected SimulatedValue Z;
+
+        //public event Action<Vector3> DesiredValueChanged;
+
+        private Interpolator.InterpolationProcedure? _interpolationProcedureX = null;
+        private Interpolator.InterpolationProcedure? _interpolationProcedureY = null;
+        private Interpolator.InterpolationProcedure? _interpolationProcedureZ = null;
+
+        public void SetCurrent(Vector3 value)
+        {
+            X.CurrentValue = value.X;
+            Y.CurrentValue = value.Y;
+            Z.CurrentValue = value.Z;
+        }
+        public void SetDesired(Vector3 value)
+        {
+            X.DesiredValue = value.X;
+            Y.DesiredValue = value.Y;
+            Z.DesiredValue = value.Z;
+        }
+
+        public void InitiateInterpolation(TimeSpan span)
+        {
+            _interpolationProcedureX = new InterpolationProcedure(X.CurrentValue, X.DesiredValue, span);
+            _interpolationProcedureY = new InterpolationProcedure(Y.CurrentValue, Y.DesiredValue, span);
+            _interpolationProcedureZ = new InterpolationProcedure(Z.CurrentValue, Z.DesiredValue, span);
+        }
+
+        public void UpdateInterpolation(Interpolator.InterpolationFunction func)
+        {
+            if (_interpolationProcedureX == null) return;
+            if (_interpolationProcedureX.GetPercent() >= 1) return;
+
+            X.CurrentValue = Interpolator.InterpolateWithProcedure(_interpolationProcedureX, func);
+            Y.CurrentValue = Interpolator.InterpolateWithProcedure(_interpolationProcedureY, func);
+            Z.CurrentValue = Interpolator.InterpolateWithProcedure(_interpolationProcedureZ, func);
+        }
+
+        public float GetInterpolationPercent()
+        {
+            if (_interpolationProcedureX == null) return 1;
+            return _interpolationProcedureX.GetPercent();
+        }
+
+        public Interpolator.InterpolationProcedure[] GetInterpolationProcedure()
+        {
+            return new InterpolationProcedure[3] { _interpolationProcedureX, _interpolationProcedureY, _interpolationProcedureZ };
         }
     }
 }
